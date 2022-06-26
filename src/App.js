d1cb0949cb13934fd5a5fd870bf8386f301de4da
mohammed quneifdi <alladqanymhmd@gmail.com>
2022-06-26 22:28:54 +0300

    it's done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React,  { useState } from 'react';
 import './App.css';
+import Button from './components/Button';
+import { Data1 , Data2} from './components/Data';
 
 function App() {
+  const [item,setItem] = useState(false);
+
+  const handleState = () => setItem(!item);
+
+  let Data;
+
+  item ? Data = Data1 : Data = Data2;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="pricing-box">
+       <div className="title-part">
+         <h2>Our Pricing</h2>
+         <div className="options">
+           <span>Annually</span>
+           <span><Button rounded={true} click={handleState} /></span>
+           <span>Monthly</span>
+         </div>
+       </div>
+       <div className="pricing-cards">
+         {
+           Data.map(items => (
+             
+            <div className="basic-card" key={items.id}> 
+              <h2 className="title">{items.title}</h2>
+              <h1 className="price">
+                <span className='d-sign'>$</span>
+                <span className='p-span'>{items.price}</span>
+              </h1>
+              <ul className="info">
+                <li>{items.line1}</li>
+                <li>{items.line2}</li>
+                <li>{items.line3}</li>
+              </ul>
+              <button className='btn'>LERN MORE</button>
+         </div>
+           ))
+         }
+       </div>
+     </div>
     </div>
   );
 }
---
+++ src/components/Data.js
@@ -0,0 +1,54 @@
+
+export const Data1 = [
+     {
+        id: 1,
+        title: "Basic",
+        price: " 19.99",
+        line1: "500 GB Storage",
+        line2: "2 Users Allowed",
+        line3: "Send up to 3 GB",
+    },
+     {
+        id: 2,
+        title: "Professional",
+        price: " 24.99",
+        line1: "1 TB Storage",
+        line2: "5 Users Allowed",
+        line3: "Send up to 10 GB",
+    },
+     {
+        id: 3,
+        title: "Master",
+        price: " 39.99",
+        line1: "2 TB Storage",
+        line2: "10 Users Allowed",
+        line3: "Send up to 20 GB",
+    }
+];
+
+export const Data2 = [
+     {
+        id: 1,
+        title: "Basic",
+        price: " 199.99",
+        line1: "500 GB Storage",
+        line2: "2 Users Allowed",
+        line3: "Send up to 3 GB",
+    },
+     {
+        id: 2,
+        title: "Professional",
+        price: " 249.99",
+        line1: "1 TB Storage",
+        line2: "5 Users Allowed",
+        line3: "Send up to 10 GB",
+    },
+     {
+        id: 3,
+        title: "Master",
+        price: " 399.99",
+        line1: "2 TB Storage",
+        line2: "10 Users Allowed",
+        line3: "Send up to 20 GB",
+    }
+];
